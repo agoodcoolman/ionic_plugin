2881b0b10a7bfb63c37de64b82800c977ae89057
DESKTOP-TTIC7FS\jin <agoodcoolman@gmail.com>
2017-07-20 10:38:32 +0800

    1.添加了 删除app的功能 2.填加构建build的功能

--- src/IonicWindowContent.java
+++ src/IonicWindowContent.java
@@ -6,10 +6,17 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import io.netty.buffer.ByteBufUtf8Writer;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.text.DefaultCaret;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,12 +26,12 @@ import java.io.*;
  * Created by jin on 2017/7/18.
  */
 public class IonicWindowContent implements ToolWindowFactory {
-    ToolWindow toolWindow;
     private JButton runButton;
     private JButton buildButton;
     private JTextArea textArea1;
     private JPanel panel1;
     private JScrollPane scrollpan;
+    private JButton uninstallButton;
 
 
     @Override
@@ -45,19 +52,14 @@ public class IonicWindowContent implements ToolWindowFactory {
                             // 获取项目下的路径。
                             String basePath = project.getBasePath();
                             Runtime runtime = Runtime.getRuntime();
-                            Process process = runtime.exec(" cmd /c ionic run android", null, new File("D:\\androidProject\\hytApp"));
+                            Process process = runtime.exec(" cmd /c ionic run android", null, new File(basePath));
 
                             InputStream in = process.getInputStream();
                             byte[] bytes = new byte[1024];
                             int length = -1;
 
                             while (-1 != (length = in.read(bytes))) {
-                                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                                byteArrayOutputStream.write(bytes);
-                                byteArrayOutputStream.flush();
-                                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream);
-                                outputStreamWriter.flush();
-//                                textArea1.write(outputStreamWriter);
+
                                 textArea1.append(new String(bytes));
                                 textArea1.setCaretPosition(textArea1.getDocument().getLength());
                                 Thread.sleep(50);
@@ -72,10 +74,90 @@ public class IonicWindowContent implements ToolWindowFactory {
                         }
                     }
                 }.start();
-
             }
         });
 
+        uninstallButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String basePath = project.getBasePath();
+                try {
+                    File configFile = new File(basePath + "\\config.xml");
+                    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                    Document document = documentBuilder.parse(configFile);
+                    NodeList widget = document.getElementsByTagName("widget");
+
+                    String packageName = widget.item(0).getAttributes().getNamedItem("id").getNodeValue();
+
+                    Runtime runtime = Runtime.getRuntime();
+                    Process process = runtime.exec(" adb uninstall " +packageName );
+
+                    InputStream in = process.getInputStream();
+                    byte[] bytes = new byte[1024];
+                    int length = -1;
+
+                    while (-1 != (length = in.read(bytes))) {
+                        new Thread(){
+                            @Override
+                            public void run() {
+                                textArea1.append(new String(bytes));
+                                textArea1.setCaretPosition(textArea1.getDocument().getLength());
+                                try {
+                                    Thread.sleep(50);
+                                } catch (InterruptedException e1) {
+                                    e1.printStackTrace();
+                                }
+                            }
+                        }.start();
+
+                    }
+
+                    process.destroy();
+
+                } catch (ParserConfigurationException e1) {
+                    e1.printStackTrace();
+                } catch (SAXException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        buildButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            // 获取项目下的路径。
+                            String basePath = project.getBasePath();
+                            Runtime runtime = Runtime.getRuntime();
+                            Process process = runtime.exec(" cmd /c ionic build android", null, new File(basePath));
+                            InputStream in = process.getInputStream();
+                            byte[] bytes = new byte[1024];
+                            int length = -1;
+
+                            while (-1 != (length = in.read(bytes))) {
+                                textArea1.append(new String(bytes));
+                                textArea1.setCaretPosition(textArea1.getDocument().getLength());
+                                Thread.sleep(50);
+                            }
+
+                            process.destroy();
+
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        });
     }
 
 
